cb98ad3b1afc9a1c2741b3e520081e56642812ad
duythien <duythien1310@gmail.com>
2024-03-04 13:39:19 +0700

    add model currency

---
+++ lib/models/currency.dart
@@ -0,0 +1,13 @@
+class Currency{
+  late String name;
+  late String code;
+  late String symbol;
+  late bool picked;
+
+  Currency({
+    required this.name,
+    required this.code,
+    required this.symbol,
+    required this.picked
+  });
+}
