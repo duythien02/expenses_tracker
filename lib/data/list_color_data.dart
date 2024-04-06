aeff78c1d530a11cebe953d1867420512eddc383
duythien <duythien1310@gmail.com>
2024-04-06 15:19:42 +0700

    chore: add color and color picker model

---
+++ lib/data/list_color_data.dart
@@ -0,0 +1,12 @@
+import 'package:expenses_tracker_app/models/my_color_picker.dart';
+import 'package:flutter/material.dart';
+
+List<MyColorPicker> colors = [
+  MyColorPicker(color: Colors.red),
+  MyColorPicker(color: Colors.orange),
+  MyColorPicker(color: Colors.yellow),
+  MyColorPicker(color: Colors.green),
+  MyColorPicker(color: Colors.blue),
+  MyColorPicker(color: Colors.indigo),
+  MyColorPicker(color: Colors.purple),
+];
