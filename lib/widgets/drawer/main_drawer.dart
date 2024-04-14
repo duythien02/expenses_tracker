c446724e38e9bc741fc8a02f607b2e469df3cc1a
duythien <duythien1310@gmail.com>
2024-04-14 14:34:16 +0700

    chore: remove sign-out in drawer

--- lib/widgets/drawer/main_drawer.dart
+++ lib/widgets/drawer/main_drawer.dart
@@ -3,12 +3,10 @@ import 'package:expenses_tracker_app/models/user.dart';
 import 'package:expenses_tracker_app/sceens/drawer/account/user_account.dart';
 import 'package:expenses_tracker_app/sceens/drawer/chart/bar_chart.dart';
 import 'package:expenses_tracker_app/sceens/drawer/chat/chat.dart';
-import 'package:expenses_tracker_app/sceens/drawer/profile.dart';
+import 'package:expenses_tracker_app/sceens/drawer/profile/profile.dart';
 import 'package:expenses_tracker_app/sceens/drawer/category/user_categories.dart';
 import 'package:expenses_tracker_app/widgets/drawer/item_drawer.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 // ignore: must_be_immutable
 class MainDrawer extends StatelessWidget {
@@ -36,7 +34,8 @@ class MainDrawer extends StatelessWidget {
                       : NetworkImage(user.image!),    
                   ),
                   accountName: Text('Tên người dùng: ${user.userName}'),
-                  accountEmail: Text('Email: ${user.email}')
+                  accountEmail: Text('Email: ${user.email}'),
+                  
                   ),
             ),
             InkWell(
@@ -75,32 +74,6 @@ class MainDrawer extends StatelessWidget {
               destination: const ChatScreen(),
               icon: Icons.message,
               title: 'Chat',
-            ),
-            InkWell(
-              onTap: () async {
-                Navigator.pop(context);
-                await showDialog(context: context, builder: (context) => AlertDialog(
-                  actions: [
-                    TextButton(onPressed: () => Navigator.pop(context), child: const Text('Huỷ')),
-                    TextButton(onPressed: () async {
-                      await FirebaseAuth.instance.signOut().whenComplete(() => Navigator.popUntil(context, (route) => route.isFirst));
-                      await GoogleSignIn().signOut();
-                    }, child: const Text('Thoát')),
-                  ],
-                  contentPadding: const EdgeInsets.all(32),
-                  content: const Text('Bạn có muốn đăng xuất không?', style: TextStyle(color: Colors.black),),
-                ));
-              },
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                child: const Row(
-                  children: [
-                    Icon(Icons.logout,color: Colors.white,),
-                    SizedBox(width: 20,),
-                    Text('Đăng xuất')
-                  ],
-                ),
-              ),
             ),
           ],
         ),
