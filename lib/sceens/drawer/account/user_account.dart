e4479e811d1c5b5bd8c5413f138ff0bf7362ae76
duythien <duythien1310@gmail.com>
2024-04-19 19:50:13 +0700

    feat: done create new account and edit account

--- lib/sceens/drawer/account/user_account.dart
+++ lib/sceens/drawer/account/user_account.dart
@@ -1,9 +1,36 @@
+import 'package:expenses_tracker_app/data/curencies_data.dart';
 import 'package:expenses_tracker_app/firebase/firebase.dart';
+import 'package:expenses_tracker_app/main.dart';
+import 'package:expenses_tracker_app/models/currency.dart';
+import 'package:expenses_tracker_app/sceens/drawer/account/create_account.dart';
+import 'package:expenses_tracker_app/widgets/account/currency_card.dart';
 import 'package:expenses_tracker_app/widgets/drawer/main_drawer.dart';
 import 'package:flutter/material.dart';
 
-class UserAccountScreen extends StatelessWidget {
+class UserAccountScreen extends StatefulWidget {
   const UserAccountScreen({super.key});
+
+  @override
+  State<UserAccountScreen> createState() => _UserAccountScreenState();
+}
+
+class _UserAccountScreenState extends State<UserAccountScreen> {
+  late Currency currency;
+  var getListAccount = FirebaseAPI.getListAccount();
+
+  @override
+  void initState() {
+    super.initState();
+    getMainAccount();
+  }
+
+  Future<void>getMainAccount() async{
+    await FirebaseAPI.getMainAccount().then((value) {
+      setState(() {
+        currency = currencies.firstWhere((element) => element.code == value.currencyCode);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +51,106 @@ class UserAccountScreen extends StatelessWidget {
           }
           return Container();
         },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 12, top: 24,left: 12, right: 12),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  width: (MediaQuery.of(context).size.width - 24) / 2,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: kColorScheme.primary
+                        ),
+                        child: const Icon(Icons.history,color: Colors.white,size: 35,),
+                      ),
+                      const SizedBox(height: 8,),
+                      const Text('Lịch sử chuyển khoản', style: TextStyle(color: Colors.black,fontSize: 14),)
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: (MediaQuery.of(context).size.width - 24) / 2,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: kColorScheme.primary
+                        ),
+                        child: const Icon(Icons.swap_horiz,color: Colors.white,size: 35,),
+                      ),
+                      const SizedBox(height: 8,),
+                      const Text('Giao dịch chuyển khoản mới', style: TextStyle(color: Colors.black,fontSize: 14),)
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24,),
+            FutureBuilder(
+              future: getListAccount,
+              builder: (context, snapshot){
+                if(snapshot.connectionState == ConnectionState.waiting){
+                  return const Expanded(
+                    child: Center(child: CircularProgressIndicator(),),
+                  );
+                }
+                if (snapshot.hasData) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) => GestureDetector(
+                        onTap: () async {
+                          var account = await Navigator.push(context, MaterialPageRoute(builder: (context) => CreateAccountScreen(currency: currency, account: snapshot.data![index])));
+                          if(account != null){
+                            setState(() {
+                              getMainAccount();
+                              getListAccount = FirebaseAPI.getListAccount();
+                            });
+                          }
+                        },
+                        child: CurrencyCard(account: snapshot.data![index]),
+                      )
+                    ),
+                  );
+                }
+                return Container();
+              },
+            ),
+            const SizedBox(height: 16,),
+            Container(
+              width: 60,
+              height: 60,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromRGBO(255, 177, 32, 1),
+              ),
+              child: IconButton(
+                color: Colors.black,
+                onPressed: () async {
+                  var account = await Navigator.push(context, MaterialPageRoute(builder: (context) => CreateAccountScreen(currency: currency,account: null,)));
+                  if(account != null){
+                    setState(() {
+                      getListAccount = FirebaseAPI.getListAccount();
+                    });
+                  }
+                },
+                icon: const Icon(Icons.add),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/account/currency_card.dart
@@ -0,0 +1,66 @@
+import 'package:expenses_tracker_app/models/account.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CurrencyCard extends StatelessWidget {
+  const CurrencyCard({super.key, required this.account});
+  final Account account;
+
+  String moneyFormat(){
+    if (account.accountBalance >= 1000000) {
+      var format = NumberFormat.compactSimpleCurrency(locale: account.currencyLocale);
+      return format.format(account.accountBalance);
+    } else {
+      var format = NumberFormat.simpleCurrency(locale: account.currencyLocale);
+      return format.format(account.accountBalance);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        height: 62,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.width / 10,
+                  width: MediaQuery.of(context).size.width / 10,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(account.color)),
+                  child: Icon(
+                    IconData(int.parse(account.symbol),
+                        fontFamily: "MyIcon"),
+                    size: MediaQuery.of(context).size.width / 16,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 3,
+                  child: Text(
+                    account.accountName,
+                    style: const TextStyle(color: Colors.black),
+                    overflow: TextOverflow.fade,
+                    softWrap: false,
+                  ),
+                ),
+              ],
+            ),
+        
+            Text(moneyFormat(),
+                    style: const TextStyle(color: Colors.black))
+          ],
+        ),
+      ),
+    );
+  }
+}
